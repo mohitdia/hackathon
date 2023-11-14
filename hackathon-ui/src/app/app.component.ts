4f4e9edd29294a08fbbd661578903683ed3f54a1
Elizabeth Ruzich <E40025915@utcaus.com>
2023-11-14 12:14:45 -0500

    hackathon got the user input working, textbox and button is ugly but meh

--- hackathon-ui/src/app/app.component.ts
+++ hackathon-ui/src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatFormFieldModule, MatInputModule, MatIconModule, 
+    MatButtonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hackathon-ui';
+  public title = 'Ask Me Anything!';
+  public userInput: string = "";
+
+  public submit(): void {
+    console.log("you clicked submit");
+    console.log("user input", this.userInput);
+  }
 }
