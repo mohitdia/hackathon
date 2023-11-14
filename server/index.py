f70d04c8a9d52fe29c5d1ae34d5d1873bdd6a721
mdia <mohit.dia@collins.com>
2023-11-14 15:26:25 -0500

    trying to serve static files

--- server/index.py
+++ server/index.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='hackathon-ui/dist/hackathon-ui/browser', static_url_path="/")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
+
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
