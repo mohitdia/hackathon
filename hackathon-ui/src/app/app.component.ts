3e2dfb37fe9e27862892b3394ba509a8fa34b80e
Shubh Sharma <shubhanshu.sharma@collins.com>
2023-11-15 13:36:35 -0500

    adding unordered list

--- hackathon-ui/src/app/app.component.ts
+++ hackathon-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
@@ -10,22 +10,25 @@ import { ButtonModule } from 'primeng/button';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from  '@angular/common/http';
 import { take } from 'rxjs/operators';
+import { ListboxModule } from 'primeng/listbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet,HttpClientModule, MatFormFieldModule, MatInputModule, MatIconModule, 
-    MatButtonModule, FormsModule, ButtonModule],
+    MatButtonModule, FormsModule, ButtonModule, ListboxModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public title = 'Ask Me Anything!';
   public userInput: string = "";
   value:string = '';
   ascentiaAnswer: string = '';
   questionAsked:string = '';
+  recentQueries: string[] = [];
+  selectedQueryFromRecentList = '';
 
   constructor(private http: HttpClient) {
     this.http.get('http://127.0.0.1:5000/getanswer', {
@@ -37,6 +40,12 @@ export class AppComponent {
     })
   }
 
+  ngOnInit() {
+    this.recentQueries = [
+        'What is Ascentia'
+    ];
+}
+
   submit(): void {
     this.questionAsked = ''
     this.ascentiaAnswer = '';
@@ -45,6 +54,7 @@ export class AppComponent {
     }).pipe(take(1)).subscribe((res:any) => {
       this.ascentiaAnswer = res.data;
       this.questionAsked = this.value;
+      this.recentQueries.push(this.value);
       this.value = '';
     })
   }
